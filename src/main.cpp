281ba84ed03c05961c82983bef5ddc80efe698aa
Afnan-Yusuf <afnanyusufpp@gmail.com>
2024-11-16 07:50:03 +0530

    no delay

--- src/main.cpp
+++ src/main.cpp
@@ -11,90 +11,96 @@ Gyro Offsets -> X: -57 Y: -148 Z: -80
 #include <Wire.h>
 
 MPU6050 mpu;
-volatile bool mpuInterrupt = false;
-uint8_t mpuIntStatus;
-uint16_t packetSize;
-uint16_t fifoCount;
-uint8_t fifoBuffer[64];
-Quaternion q;
-float euler[3];
-void handleFIFOData();
-void IRAM_ATTR dmpDataReady()
+
+#define INTERRUPT_PIN 2 // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13      // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+Quaternion q;        // [w, x, y, z]         quaternion container
+VectorInt16 aa;      // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;  // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld; // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity; // [x, y, z]            gravity vector
+float euler[3];      // [psi, theta, phi]    Euler angle container
+float ypr[3];        // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
+
+volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
 {
   mpuInterrupt = true;
 }
-
 void setup()
 {
   Wire.begin(21, 22);
-  Wire.setClock(100000);
+  Wire.setClock(400000);
   Serial.begin(115200);
-
-  Serial.println("Initializing MPU6050...");
   mpu.initialize();
-  if (!mpu.testConnection())
-  {
-    Serial.println("MPU6050 connection failed");
-    while (1)
-      ;
-  }
+  pinMode(INTERRUPT_PIN, INPUT);
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-  Serial.println("Initializing DMP...");
-  if (mpu.dmpInitialize() == 0)
+  devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788);
+
+  if (devStatus == 0)
   {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    Serial.println(F("Enabling DMP..."));
     mpu.setDMPEnabled(true);
-    attachInterrupt(digitalPinToInterrupt(2), dmpDataReady, RISING);
-    mpu.resetFIFO();
+
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
     packetSize = mpu.dmpGetFIFOPacketSize();
-    Serial.println("DMP ready!");
   }
   else
   {
-    Serial.println("DMP Initialization failed");
-    while (1)
-      ;
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
   }
-
-  mpu.setXAccelOffset(-711);
-  mpu.setYAccelOffset(-277);
-  mpu.setZAccelOffset(1067);
-  mpu.setXGyroOffset(-57);
-  mpu.setYGyroOffset(-148);
-  mpu.setZGyroOffset(-80);
-  mpu.setDLPFMode(0);
-  mpu.setRate(200);
 }
 
 void loop()
 {
-  if (!mpuInterrupt)
+  if (!dmpReady)
     return;
-
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-  handleFIFOData();
-}
-
-void handleFIFOData()
-{
-  fifoCount = mpu.getFIFOCount();
-
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024)
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
   {
-    mpu.resetFIFO();
-    Serial.println("FIFO overflow!");
-    return;
-  }
-
-  while (fifoCount >= packetSize)
-  {
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-    fifoCount -= packetSize;
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetEuler(euler, &q);
-
-
-    Serial.println(euler[1] * 180 / M_PI);
-
+    Serial.print("euler\t");
+    Serial.print(euler[0] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(euler[1] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.println(euler[2] * 180 / M_PI);
   }
 }
+
+/**/
